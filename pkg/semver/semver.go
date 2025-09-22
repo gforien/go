96e8164f6f5564ee7b3218aca127f8365ed0fc3d
gforien <gforien+dev@gmail.com>
2025-09-22 18:27:53 +0200

    feat(semver): init pkg/semver

---
+++ pkg/semver/semver.go
@@ -0,0 +1,138 @@
+package semver
+
+import (
+	"fmt"
+	"regexp"
+	"strconv"
+)
+
+// Version is a semver version with format Major.minor.patch
+// An zero-valued Version behaves likes 0.0.0
+type Version struct {
+	Major int
+	Minor int
+	Patch int
+}
+
+var zero = &Version{0, 0, 0}
+
+var semver = regexp.MustCompile(`(\d+)\.(\d+)\.(\d+)`)
+
+// We expect exactly 3 matches to be found when parsing
+// a provided string with the semver regexp
+// If the provided string does not meet this criteria, ErrCannotParse is returned.
+type ErrCannotParse struct {
+	message string
+}
+
+func (e *ErrCannotParse) Error() string {
+	return e.message
+}
+
+func FromString(s string) (Version, error) {
+	// Regular expression to capture version numbers
+	matches := semver.FindStringSubmatch(s)
+	if len(matches) != 4 {
+		err := &ErrCannotParse{
+			message: fmt.Sprintf("Cannot parse '%s' into semver (found %d matches)", s, len(matches)),
+		}
+		return Version{}, err
+	}
+
+	// Convert matches to integers
+	major, _ := strconv.Atoi(matches[1])
+	minor, _ := strconv.Atoi(matches[2])
+	patch, _ := strconv.Atoi(matches[3])
+	return Version{Major: major, Minor: minor, Patch: patch}, nil
+}
+
+func (s *Version) String() string {
+	if s == nil {
+		return zero.String()
+	}
+
+	return fmt.Sprintf("%d.%d.%d", s.Major, s.Minor, s.Patch)
+}
+
+func (s *Version) NextMajor() *Version {
+	if s == nil {
+		return zero.NextMajor()
+	}
+
+	return &Version{
+		Major: s.Major + 1,
+		Minor: 0,
+		Patch: 0,
+	}
+}
+
+func (s *Version) NextMinor() *Version {
+	if s == nil {
+		return zero.NextMinor()
+	}
+
+	return &Version{
+		Major: s.Major,
+		Minor: s.Minor + 1,
+		Patch: 0,
+	}
+}
+
+func (s *Version) NextPatch() *Version {
+	if s == nil {
+		return zero.NextPatch()
+	}
+
+	return &Version{
+		Major: s.Major,
+		Minor: s.Minor,
+		Patch: s.Patch + 1,
+	}
+}
+
+// Compare returns
+// -1 if the version is less than the other version,
+// 0 if they are equal,
+// +1 if the version is greater than the other version.
+func (s *Version) Compare(other *Version) int {
+	if s == nil {
+		return zero.Compare(other)
+	}
+	if other == nil {
+		return s.Compare(zero)
+	}
+
+	if (s.Major > other.Major) ||
+		(s.Major == other.Major && s.Minor > other.Minor) ||
+		(s.Major == other.Major && s.Minor == other.Minor && s.Patch > other.Patch) {
+		return 1
+	}
+
+	if (s.Major < other.Major) ||
+		(s.Major == other.Major && s.Minor < other.Minor) ||
+		(s.Major == other.Major && s.Minor == other.Minor && s.Patch < other.Patch) {
+		return -1
+	}
+
+	return 0
+}
+
+func (s *Version) GreaterThan(other *Version) bool {
+	return s.Compare(other) > 0
+}
+
+func (s *Version) GreaterOrEqual(other *Version) bool {
+	return s.Compare(other) >= 0
+}
+
+func (s *Version) LessThan(other *Version) bool {
+	return s.Compare(other) < 0
+}
+
+func (s *Version) LessOrEqual(other *Version) bool {
+	return s.Compare(other) <= 0
+}
+
+func (s *Version) Equals(other *Version) bool {
+	return s.Compare(other) == 0
+}
---
+++ pkg/semver/semver_test.go
@@ -0,0 +1,122 @@
+package semver
+
+import (
+	"testing"
+)
+
+func TestFromString(t *testing.T) {
+	cases := []struct {
+		given    string
+		expected Version
+	}{
+		{
+			given:    "1.2.3",
+			expected: Version{Major: 1, Minor: 2, Patch: 3},
+		},
+		{
+			given:    "0.0.1",
+			expected: Version{Major: 0, Minor: 0, Patch: 1},
+		},
+		{
+			given:    "10.0.0",
+			expected: Version{Major: 10, Minor: 0, Patch: 0},
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.given, func(t *testing.T) {
+			t.Parallel()
+
+			res, err := FromString(tc.given)
+			if err != nil {
+				t.Errorf("expected no error, but got %#v", err)
+			}
+			if res != tc.expected {
+				t.Errorf("expected %#v, but got %#v", tc.expected, res)
+			}
+		})
+	}
+}
+
+func TestFromStringFailing(t *testing.T) {
+	cases := []struct {
+		given    string
+		expected Version
+	}{
+		{given: "", expected: Version{}},
+		{given: "1", expected: Version{}},
+		{given: "1.2", expected: Version{}},
+		{given: "a.b.c", expected: Version{}},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.given, func(t *testing.T) {
+			t.Parallel()
+
+			res, err := FromString(tc.given)
+			if err == nil {
+				t.Errorf("expected an error %#v but got none", tc.expected)
+			}
+			if res != tc.expected {
+				t.Errorf("expected %#v, but got %#v", tc.expected, res)
+			}
+		})
+	}
+}
+
+func TestString(t *testing.T) {
+	t.Parallel()
+
+	v := &Version{Major: 1, Minor: 2, Patch: 3}
+	expected := "1.2.3"
+	if v.String() != expected {
+		t.Errorf("Version.String(): expected %s, got %s", expected, v.String())
+	}
+}
+
+func TestNextMajor(t *testing.T) {
+	t.Parallel()
+
+	v := &Version{Major: 1, Minor: 2, Patch: 3}
+	expected := &Version{Major: 2, Minor: 0, Patch: 0}
+	result := v.NextMajor()
+	if !expected.Equals(result) {
+		t.Errorf("Version.ReleaseMajor(): expected %v, got %v", expected, result)
+	}
+}
+
+func TestNextMinor(t *testing.T) {
+	t.Parallel()
+
+	v := &Version{Major: 1, Minor: 2, Patch: 3}
+	expected := &Version{Major: 1, Minor: 3, Patch: 0}
+	result := v.NextMinor()
+	if !expected.Equals(result) {
+		t.Errorf("Version.ReleaseMinor(): expected %v, got %v", expected, result)
+	}
+}
+
+func TestNextPatch(t *testing.T) {
+	t.Parallel()
+
+	v := &Version{Major: 1, Minor: 2, Patch: 3}
+	expected := &Version{Major: 1, Minor: 2, Patch: 4}
+	result := v.NextPatch()
+	if !expected.Equals(result) {
+		t.Errorf("Version.ReleasePatch(): expected %v, got %v", expected, result)
+	}
+}
+
+func TestErrCannotParse(t *testing.T) {
+	t.Parallel()
+
+	input := "1.2"
+	_, err := FromString(input)
+	if err == nil {
+		t.Fatalf("FromString(%s): expected error, got nil", input)
+	}
+
+	if _, ok := err.(*ErrCannotParse); !ok {
+		t.Fatalf("FromString(%s): expected *ErrCannotParse, got %T", input, err)
+	}
+}
